70dace41a335e9288e83f4a097bc10c699fa5fbc
Dmitry Guzeev <dmitry@evrone.com>
2024-05-17 20:48:42 +0300

    header update

--- wp-content/themes/test-theme/header.php
+++ wp-content/themes/test-theme/header.php
@@ -6,3 +6,14 @@
     <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?>> 
+    <header class="b-header">
+      <div class="b-header__container">
+        <a href="/"><h2>Header</h2></a>
+      </div>
+      <?php wp_nav_menu( array(
+        'theme_location' => 'main',
+        'container'       => 'nav',
+        'container_class' => 'b-header__container',
+        )
+      );?>
+    </header>
